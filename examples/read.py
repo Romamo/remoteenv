93cff7e1a33a744e68850d4a0f2ac2cf6fe486fb
Romamo <romamo@yandex.ru>
2024-12-23 19:58:44 +0200

    New version

--- examples/read.py
+++
@@ -1,8 +0,0 @@
-from remoteenv.zoo import ZooEnv
-
-
-env = ZooEnv('prefix', 'zoohost:2181')
-if env.start():
-    for k, v in env.read('mod', 'h4', 'mod/h4'):
-        print(f"{k}={v}")
-    env.stop()
--- remoteenv/zoo/__init__.py
+++
@@ -1,3 +0,0 @@
-from .env import ZooEnv
-
-__all__ = ['ZooEnv']
